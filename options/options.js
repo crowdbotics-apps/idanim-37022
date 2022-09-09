413e43607238f165d9db3d3b99bdc83bcab41244
Crowdbotics <team@crowdbotics.com>
2022-09-09 09:03:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "idanim_37022",
+  url: "https://idanim_37022.botics.co",
+  api: "https://idanim_37022.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
